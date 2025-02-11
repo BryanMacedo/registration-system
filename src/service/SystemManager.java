1b70476fa219565a1568b8f15cca4c4d1e4ac843
Bryan Macedo <contato.bryansm@hotmail.com>
2025-02-11 15:50:21 -0300

    Criei um menu na classe Main para mostrar as opções da aplicação e criei a classe SystemManager para gerenciar os recursos da aplicação.

---
+++ src/service/SystemManager.java
@@ -0,0 +1,77 @@
+package service;
+
+import domain.User;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class SystemManager {
+    private List<User> users = new ArrayList<>();
+    private List<String> mainQuestions = new ArrayList<>();
+
+    Scanner sc = new Scanner(System.in);
+
+    public void readQuestions(){
+        String pathFormTxt = "D:\\java-estudos\\registration-system\\src\\resource\\formulario.txt";
+
+        try (BufferedReader bReader = new BufferedReader(new FileReader(pathFormTxt))) {
+            String line;
+            while ((line = bReader.readLine()) != null) {
+                mainQuestions.add(line);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void registerUser(){
+        if (mainQuestions.isEmpty()){
+            readQuestions();
+        }
+        System.out.println("--- Cadastrando um usuário ---");
+        System.out.print(mainQuestions.get(0) + " ");
+        String fullName = sc.nextLine();
+
+        System.out.print(mainQuestions.get(1) + " ");
+        String email = sc.nextLine();
+
+        System.out.print(mainQuestions.get(2) + " ");
+        int age = sc.nextInt();
+
+        System.out.print(mainQuestions.get(3) + " ");
+        double height = sc.nextDouble();
+
+        User user = new User(fullName, email, age, height);
+        users.add(user);
+
+        System.out.println(users.get(0));
+
+        String fileName = ("1-" + users.get(0).getFullName().toUpperCase() + ".txt").replaceAll(" ", "");
+        String folderPath = "D:\\java-estudos\\registration-system\\src\\userDirectory";
+
+        File directory = new File(folderPath);
+        if (!directory.exists()) {
+            directory.mkdir();
+        }
+
+        File userFile = new File(folderPath, fileName);
+
+        try (BufferedWriter bWriter = new BufferedWriter(new FileWriter(userFile))) {
+            bWriter.write(users.get(0).getFullName());
+            bWriter.newLine();
+            bWriter.write(users.get(0).getEmail());
+            bWriter.newLine();
+            bWriter.write(String.valueOf(users.get(0).getAge()));
+            bWriter.newLine();
+            bWriter.write(String.valueOf(users.get(0).getHeight()));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        sc.close();
+    }
+}
--- src/test/Main.java
+++ src/test/Main.java
@@ -1,70 +1,29 @@
 package test;
 
-import domain.User;
+import service.SystemManager;
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        SystemManager systemManager = new SystemManager();
         Scanner sc = new Scanner(System.in);
-        String pathFormTxt = "D:\\java-estudos\\registration-system\\src\\resource\\formulario.txt";
-        List<User> users = new ArrayList<>();
+        System.out.println("--- SISTEMA DE CADASTRO ---");
+        System.out.println("1 - Cadastrar o usuário.");
+        System.out.println("2 - Listar todos usuários cadastrados");
+        System.out.println("3 - Cadastrar nova pergunta no formulário");
+        System.out.println("4 - Deletar pergunta do formulário");
+        System.out.println("5 - Pesquisar usuário por nome ou idade ou email");
+        System.out.print("Escolha: ");
+        int choice = sc.nextInt();
 
-        List<String> mainQuestions = new ArrayList<>();
-        try (BufferedReader bReader = new BufferedReader(new FileReader(pathFormTxt))) {
-            String line;
-            while ((line = bReader.readLine()) != null) {
-                mainQuestions.add(line);
+        switch (choice){
+            case 1 -> {
+                systemManager.registerUser();
             }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("--- Cadastrando um usuário ---");
-        System.out.print(mainQuestions.get(0) + " ");
-        String fullName = sc.nextLine();
-
-        System.out.print(mainQuestions.get(1) + " ");
-        String email = sc.nextLine();
-
-        System.out.print(mainQuestions.get(2) + " ");
-        int age = sc.nextInt();
-
-        System.out.print(mainQuestions.get(3) + " ");
-        double height = sc.nextDouble();
-
-        User user = new User(fullName, email, age, height);
-        users.add(user);
-
-        System.out.println(users.get(0));
-
-        String fileName = ("1-"+users.get(0).getFullName().toUpperCase()+".txt").replaceAll(" ", "");
-        String folderPath = "D:\\java-estudos\\registration-system\\src\\userDirectory";String filePath = folderPath + fileName;
-
-        File directory = new File(folderPath);
-        if (!directory.exists()){
-            directory.mkdir();
-        }
-
-        File userFile = new File(folderPath, fileName);
-
-        try (BufferedWriter bWriter = new BufferedWriter(new FileWriter(userFile))){
-            bWriter.write(users.get(0).getFullName());
-            bWriter.newLine();
-            bWriter.write(users.get(0).getEmail());
-            bWriter.newLine();
-            bWriter.write(String.valueOf(users.get(0).getAge()));
-            bWriter.newLine();
-            bWriter.write(String.valueOf(users.get(0).getHeight()));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
 
         sc.close();
+
     }
 }
